9e1a253cb529a915e39834eb3dd0a021b938660b
gayathrijayaram <gayathri.jayaram@ccs-technologies.com>
2021-03-15 11:34:00 +0530

    Initial commit of balance stock update and route map view.

---
+++ BalanceStockUpdate/src/webparts/balanceStock/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBalanceStockWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BalanceStockWebPartStrings' {
+  const strings: IBalanceStockWebPartStrings;
+  export = strings;
+}
